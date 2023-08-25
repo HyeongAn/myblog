acf17cec60eaa8fb64c93f6850edd58f6c2b0987
hyeongAn <guddks92@p4ace.com>
2023-08-25 22:33:49 +0900

    daily commit

--- myblog/src/components/home/slider.tsx
+++ myblog/src/components/home/slider.tsx
@@ -1,50 +1,39 @@
 'use client'
 import { useEffect, useRef, useState } from 'react'
-import { RowCenterContainer } from '../style/container'
+import { CardSlider, RowCenterContainer } from '../style/container'
 import SliderCard from './slider-card'
 import { SliderProps } from '../../../types/props'
 
 const Slider = ({ postData }: SliderProps) => {
   const slideRef = useRef<HTMLDivElement>(null)
+  const [currentIndx, setCurrentIndx] = useState(0)
+  const [count, setCount] = useState(0)
+  const total_slides = postData.length - 1
+
+  useEffect(() => {
+    const slideCurrent = slideRef.current
+    if (slideCurrent) {
+      slideCurrent.style.transition = 'all 0.5s ease-in-out'
+      slideCurrent.style.transform = `translateX(-${currentIndx * 33.3}%)`
+    }
+  }, [currentIndx])
 
   const onPrevClick = () => {
-    const slide = slideRef.current
-    if (slide) {
-      slide.scrollLeft -= slide.offsetWidth
-      if (slide.scrollLeft <= 0) slide.scrollLeft = slide.scrollWidth
-    }
+    if (currentIndx === 0) setCurrentIndx(total_slides)
+    else setCurrentIndx((prev) => prev - 1)
   }
 
   const onNextClick = () => {
-    const slide = slideRef.current
-    if (slide) {
-      slide.scrollLeft += slide.offsetWidth
-      if (slide.scrollLeft >= slide.scrollWidth - slide.offsetWidth) slide.scrollLeft = 0
-    }
+    if (currentIndx >= total_slides) setCurrentIndx(0)
+    else setCurrentIndx((prev) => prev + 1)
   }
 
-  useEffect(() => {
-    setInterval(onNextClick, 2000)
-  }, [])
-
   return (
-    <RowCenterContainer style={{ width: '100%', height: '500px' }}>
+    <RowCenterContainer style={{ maxWidth: '1163px', width: '100%', height: '500px', overflow: 'hidden' }}>
       <button onClick={() => onPrevClick()}>prev</button>
-      <div
-        ref={slideRef}
-        style={{
-          maxWidth: '1080px',
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <CardSlider ref={slideRef}>
         {postData && postData.map((item, idx) => <SliderCard data={item} key={`slide-card-${idx}`} />)}
-      </div>
-
+      </CardSlider>
       <button onClick={() => onNextClick()}>next</button>
     </RowCenterContainer>
   )
