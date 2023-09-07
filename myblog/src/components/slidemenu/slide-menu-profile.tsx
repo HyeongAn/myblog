678b4f648ddecfbf3768ca56eb94c4e5bae5e4cb
HyeongAn <guddks84@gmail.com>
2023-09-07 15:02:21 +0900

    hover effect icon aria describedby

--- myblog/src/components/slidemenu/slide-menu-profile.tsx
+++ myblog/src/components/slidemenu/slide-menu-profile.tsx
@@ -22,26 +22,22 @@ const SlideMenuProfile = () => {
             href: 'https://www.notion.so/87d3f1d26daf49368f9dfe7119f2cb84?pvs=4',
             src: notionIcon,
             alt: 'notion icon',
+            name: 'Notion',
           },
           1: {
             href: 'https://github.com/HyeongAn',
             src: gitIcon,
             alt: 'git icon',
+            name: 'Git',
           },
           2: {
             href: 'https://velog.io/@ahsy92',
             src: velogIcon,
             alt: 'velog icon',
+            name: 'Velog',
           },
         }).map((menu, index) => {
-          return (
-            <ProfileIconButtons
-              key={`menu-list-index-${index}`}
-              src={menu.src}
-              href={menu.href}
-              alt={menu.alt}
-            />
-          )
+          return <ProfileIconButtons key={`menu-list-index-${index}`} profile={menu} />
         })}
       </RowBetweenContainer>
     </>
--- myblog/src/components/style/buttons/profile-icon-button.tsx
+++ myblog/src/components/style/buttons/profile-icon-button.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import { ProfileIconButtonProps } from '../../../../types/props'
+import { IconLink, IconLinkContainer } from './button'
 
-const ProfileIconButtons = ({ href, src, alt }: ProfileIconButtonProps) => {
+const ProfileIconButtons = ({ profile }: ProfileIconButtonProps) => {
   return (
-    <a href={href}>
-      <Image src={src} alt={alt} style={{ width: '18px', height: '18px' }} />
-    </a>
+    <IconLinkContainer>
+      <IconLink aria-describedby="icon-info" target="_blank" href={profile.href}>
+        <Image src={profile.src} alt={profile.alt} style={{ width: '18px', height: '18px' }} />
+      </IconLink>
+      <span role="tooltip" id="icon-info">
+        {profile.name}
+      </span>
+    </IconLinkContainer>
   )
 }
 
